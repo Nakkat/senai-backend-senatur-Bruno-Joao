2a1bb2a6e827a6b2cc96a6c4518d02a2c281bd61
Bruno Eidy Nakamura <51564241807@info.local>
2020-03-10 09:18:14 -0300

    Banco Criado

---
+++ BD/03_SELECAO_BD.sql
@@ -0,0 +1,9 @@
+USE Senatur_Manha
+GO
+
+SELECT IdTipoUsuario, Titulo FROM tipoUsuario
+
+SELECT IdUsuario, Email, Senha, tu.IdTipoUsuario, Titulo FROM Usuario u
+INNER JOIN tipoUsuario tu on tu.IdTipoUsuario = u.IdTipoUsuario
+
+SELECT IdPacote, NomePacote, Descricao, DataIda, DataVolta, Valor, Ativo, NomeCidade FROM Pacote
